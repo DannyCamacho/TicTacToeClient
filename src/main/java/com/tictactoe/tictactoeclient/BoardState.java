e3a32456c6ee711539272c10eb06de020661916f
Daniel Camacho <dcamacho4@saddleback.edu>
2022-05-10 22:24:11 -0700

    adjustments to Chat Messaging

--- src/main/java/com/tictactoe/tictactoeclient/BoardState.java
+++ src/main/java/com/tictactoe/tictactoeclient/BoardState.java
@@ -5,8 +5,10 @@ public class BoardState {
     private char playerToken;
     private char currentToken;
 
-    public BoardState(char [] board) {
-        this.board = board;
+    public BoardState() {
+        board = new char[9];
+        playerToken = 0;
+        currentToken = 0;
     }
 
     public void setBoard(char[] board) {
@@ -35,23 +37,5 @@ public class BoardState {
 
     public boolean isPlayerTurn() {
         return playerToken == currentToken;
-    }
-
-    public String endStateHistory(String result) {
-        String s = switch (result) {
-            case "X" -> "\t\tPlayer X Win\n";
-            case "O" -> "\t\tPlayer O Win\n";
-            default -> "\t\tDraw Game\n";
-        };
-
-        return (board[0] == 0 ? "  " : board[0]) + "  |  " +
-               (board[1] == 0 ? "  " : board[1]) + "  |  " +
-               (board[2] == 0 ? "  " : board[2]) + "\n" +
-               (board[3] == 0 ? "  " : board[3]) + "  |  " +
-               (board[4] == 0 ? "  " : board[4]) + "  |  " +
-               (board[5] == 0 ? "  " : board[5]) + s +
-               (board[6] == 0 ? "  " : board[6]) + "  |  " +
-               (board[7] == 0 ? "  " : board[7]) + "  |  " +
-               (board[8] == 0 ? "  " : board[8]);
     }
 }
